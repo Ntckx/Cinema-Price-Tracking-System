562f86a7d6e023f784a84b95b99b19b306ea346a
Ntckx <Nontakorn.chatk@gmail.com>
2026-05-11 16:43:47 +0700

    edit Global Exception Handler

--- src/main/java/com/ntck/cinematickettrackingsystem/GlobalException/GlobalExceptionHandler.java
+++ src/main/java/com/ntck/cinematickettrackingsystem/GlobalException/GlobalExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
-public class GlobalException {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(BookingNotFoundException.class)
     public ProblemDetail handleBookingNotFound(BookingNotFoundException e){
